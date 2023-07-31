b59ea0d9d87eb962042dd1e123ea02c500368ebd
Zintle-zee <127285917+Zintle-zee@users.noreply.github.com>
2023-07-31 15:42:12 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+let now = new Date();
+
+let h4 = document.querySelector("h4");
+let date = now.getDate();
+let hour = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+let day = days[now.getDay()];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+let month = months[now.getMonth()];
+h4.innerHTML = ` ${date} ${month} ${year}, ${day}, ${hour}:${minutes} `;
+
 function showWeather(response) {
     let temperature = Math.round(response.data.main.temp);
     let city = response.data.name;
